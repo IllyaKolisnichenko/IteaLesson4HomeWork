a724bbaf47cf6a4c8c6b16f98f68c8345c1b50d4
slavamishch1979@gmail.com <slavamishch1979@gmail.com>
2022-06-12 17:41:03 +0300

    pyramida

---
+++ pyramida/pyramida/pyramida.cpp
@@ -0,0 +1,30 @@
+﻿// pyramida.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string s;
+    char symbol;
+    std::cout << "Vvedite simvol -";
+    std::cin >> symbol;
+    std::cout << std::endl;
+    std::cout << "Vvedite visotu pyramydi(max 50) -";
+    int high;
+    s = symbol;
+    s = symbol+s;
+    std::cin >> high;
+    
+    for (int i{}; i < high; i++)
+    {
+        for (int j = 1; j < high - i; j++)
+           std:: cout << " ";
+       
+        std::cout << s;
+        s = symbol + s + symbol;
+        std::cout << std::endl;
+    }
+    return 0;
+}
+
