09c8b6e607d957bb9e40cd526a95a165c04e9b2c
slavamishch1979@gmail.com <slavamishch1979@gmail.com>
2022-06-12 17:56:35 +0300

    pyramida

--- pyramida/pyramida/pyramida.cpp
+++ pyramida/pyramida/pyramida.cpp
@@ -4,27 +4,35 @@
 #include <iostream>
 #include <string>
 
+std::string s;
+char symbol;
+int high;
+
+void buildPyramid(char symbol, int high)
+{
+    for (int i{}; i < high; i++)
+    {
+        for (int j = 1; j < high - i; j++)
+            std::cout << " ";
+
+        std::cout << s;
+        s = symbol + s + symbol;
+        std::cout << std::endl;
+    }
+
+}
+
 int main() {
-    std::string s;
-    char symbol;
+   
     std::cout << "Vvedite simvol -";
     std::cin >> symbol;
     s = symbol;
     s = symbol + s;
     std::cout << "Vvedite visotu pyramydi(max 50) -";
-    int high;
     std::cin >> high;
     std::cout << std::endl;
     
-    for (int i{}; i < high; i++)
-    {
-        for (int j = 1; j < high - i; j++)
-           std:: cout << " ";
-       
-        std::cout << s;
-        s = symbol + s + symbol;
-        std::cout << std::endl;
-    }
+    buildPyramid(symbol, high);
     return 0;
 }
 
