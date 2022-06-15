f84e58eabe7433ee8233802c8213a85338fe3bf1
vngbro <vladimir.golubenko@gmail.com>
2022-06-15 23:14:42 +0300

    piramida

---
+++ piramid.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+int main()
+{
+    int size;
+    char block;
+    std::cout << "Enter the size of the new pyramid: ";
+    std::cin >> size;
+    std::cout << "\nEnter the symbol to build the pyramid: ";
+    std::cin >> block;
+    for (int i = 0; i <= size; ++i)
+    {
+        for (int j = 0; j <= size - i; ++j)
+        {
+            std::cout << " ";
+        }
+        for (int j = (size - 2 * i)+1; j <= size; ++j)
+        {
+            std::cout << block;
+        }
+        std::cout << "\n";
+    }
+    return 0;
+}
